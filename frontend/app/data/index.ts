7f65e4a4921b88e0bca78a98715aa6b86ef76d64
opepraise <praiseopeyemi28@gmail.com>
2026-05-17 23:53:04 +0100

    data: add barrel index.ts re-exporting all data modules

---
+++ frontend/app/data/index.ts
@@ -0,0 +1,7 @@
+export * from "./constants";
+export * from "./contracts";
+export * from "./lifecycle";
+export * from "./nav-links";
+export * from "./params";
+export * from "./security";
+export * from "./use-cases";
