e3f59a8a61b8cc40383c141300375caf9aa08a42
opepraise <praiseopeyemi28@gmail.com>
2026-05-21 08:58:30 +0100

    feat: export architecture data from data barrel

--- frontend/app/data/index.ts
+++ frontend/app/data/index.ts
@@ -1,5 +1,6 @@
 export * from "./constants";
 export * from "./contracts";
+export * from "./architecture";
 export * from "./how-it-works";
 export * from "./lifecycle";
 export * from "./nav-links";
