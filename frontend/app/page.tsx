4591c8f068d23a26cc3f09d59390a7ff5a05734d
opepraise <praiseopeyemi28@gmail.com>
2026-05-21 09:02:22 +0100

    feat: add Roadmap section to page layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,6 +11,7 @@ import {
   Contracts,
   Security,
   TechStack,
+  Roadmap,
   CTASection,
   OpenSourceBanner,
   BackToTop,
@@ -37,6 +38,7 @@ export default function Home() {
         <Contracts />
         <Security />
         <TechStack />
+        <Roadmap />
         <CTASection />
         <OpenSourceBanner />
         <Footer />
