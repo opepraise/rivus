ac4bacfabd7227860d1b5d96da5a0acfeb95d361
opepraise <praiseopeyemi28@gmail.com>
2026-05-21 09:04:47 +0100

    feat: add SDK npm and GitHub URL constants

--- frontend/app/data/constants.ts
+++ frontend/app/data/constants.ts
@@ -1,2 +1,4 @@
 export const GITHUB_URL = "https://github.com/opepraise/rivus";
 export const CONTRACTS_URL = "https://github.com/opepraise/rivus/tree/main/contracts";
+export const SDK_NPM_URL = "https://www.npmjs.com/package/rivus-sdk";
+export const SDK_GITHUB_URL = "https://github.com/opepraise/rivus-sdk";
