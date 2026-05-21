2607e7fb61c04aa7b0a6373a826a6e010a6d65ab
opepraise <praiseopeyemi28@gmail.com>
2026-05-21 08:59:40 +0100

    feat: export HowItWorks and HowItWorksStep from components barrel

--- frontend/app/components/index.ts
+++ frontend/app/components/index.ts
@@ -20,3 +20,5 @@ export * from "./CTASection";
 export * from "./OpenSourceBanner";
 export * from "./BackToTop";
 export * from "./Footer";
+export * from "./HowItWorks";
+export * from "./HowItWorksStep";
