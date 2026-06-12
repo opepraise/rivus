71bdba24223b96488c58239fb9845f5f3ed276ea
opepraise <praiseopeyemi28@gmail.com>
2026-06-12 11:44:30 +0100

    fix(icons): export ExternalLinkIcon from icons barrel

--- frontend/app/components/icons/index.ts
+++ frontend/app/components/icons/index.ts
@@ -2,4 +2,5 @@ export * from "./ArrowIcon";
 export * from "./CheckIcon";
 export * from "./ChevronDownIcon";
 export * from "./CopyIcon";
+export * from "./ExternalLinkIcon";
 export * from "./GitHubIcon";
